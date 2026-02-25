2b44650ba18e3b31cd9b27fc42fc8a0e6b21ae7f
Prince Jarial <158800256+princejarial@users.noreply.github.com>
2026-02-25 10:56:52 +0530

    Time: 27 ms (91.30%) | Memory: 184.7 MB (22.43%) - LeetSync

---
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        
+        int[] freq = new int[101];
+        
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            
+            freq[nums[right] + 50]++;
+            
+            if (right - left + 1 == k) {
+                
+                int count = 0;
+                int beauty = 0;
+
+                for (int i = 0; i < 50; i++) {
+                    count += freq[i];
+                    
+                    if (count >= x) {
+                        beauty = i - 50;
+                        break;
+                    }
+                }
+                
+                result[left] = beauty;
+                
+                freq[nums[left] + 50]--;
+                
+                left++;
+            }
+        }
+        
+        return result;
+    }
+}
