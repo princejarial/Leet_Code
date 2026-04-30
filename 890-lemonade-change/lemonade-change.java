e4e20043ba26643349a647139234cde798bc6fd6
Prince Jarial <158800256+princejarial@users.noreply.github.com>
2026-04-30 11:46:03 +0530

    Time: 2 ms (99.65%) | Memory: 72.6 MB (29.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int five = 0;  
-        int ten = 0;  
-        
-        for (int bill : bills) {
-            if (bill == 5) {
+        int five =0;
+        int ten =0;
+
+        for(int bill : bills){
+            if(bill ==5){
                 five++;
-            } 
-            else if (bill == 10) {
-                if (five == 0) return false;
+            }
+            else if(bill ==10){
+                if(five==0)
+                return false;
                 five--;
                 ten++;
-            } 
-            else {
-                if (ten > 0 && five > 0) {
+            }
+            else{
+                if(ten > 0 && five >0){
                     ten--;
                     five--;
-                } else if (five >= 3) {
+                }
+                else if(five >= 3){
                     five -= 3;
-                } else {
+                } else{
                     return false;
                 }
             }
         }
-        
         return true;
     }
 }
